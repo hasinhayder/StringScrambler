fd196067c4df1278a431acbc620c93c10d7557f8
Hasin Hayder <countdraculla@gmail.com>
2017-09-20 03:22:32 +0600

    App description

--- StringScrambler.go
+++ StringScrambler.go
@@ -1,5 +1,7 @@
 /**
- * Aoccdrnig to a rscheearch at Cmabrigde Uinervtisy, it deosn't mttaer in waht oredr the ltteers in a wrod are, the olny iprmoetnt tihng is taht the frist and lsat ltteer be at the rghit pclae. The rset can be a toatl mses and you can sitll raed it wouthit porbelm. Tihs is bcuseae the huamn mnid deos not raed ervey lteter by istlef, but the wrod as a wlohe.
+ * String Scrambler
+ * It will sohw you how amzingaly yuor biarn wkors, bbay!
+ *
  * Hasin Hayder <me at hasin dot me>
  */
 package main
